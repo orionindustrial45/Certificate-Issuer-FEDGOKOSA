93de662ab74e850e516785d86ab5e54abf9c31d9
Hilary Titus Naor <hilarytn@gmail.com>
2024-04-24 11:50:15 +0100

    Re arrange files

--- cert-app/routes/applicationRoutes.js
+++ cert-app/routes/applicationRoutes.js
@@ -16,7 +16,7 @@ const router = express.Router();
 router.post('/applications', createApplication);
 
 // Route to retrieve all applications (pending by default)
-router.get('/get-applicants', authenticate, getApplicants);
+router.get('/get-applicants', getApplicants);
 
 // Route to approve an application by ID
 router.patch('/applications/:id/approve', authenticate, approveApplication);
@@ -25,7 +25,7 @@ router.patch('/applications/:id/approve', authenticate, approveApplication);
 router.get('/approved-applicants', authenticate, getApprovedApplicants);
 
 //Route to retrieve a list of issued applicants
-router.get('/issued-applicants', authenticate, getIsssuedApplicants)
+router.get('/issued-applicants', getIsssuedApplicants)
 
 
 
