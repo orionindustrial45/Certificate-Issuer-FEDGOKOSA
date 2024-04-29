630736f613dde46b22acb9efefe8faa4cfca4a68
orionindustrial45 <166864535+orionindustrial45@users.noreply.github.com>
2024-04-29 15:07:30 +0100

    Update authRoutes.js

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -4,7 +4,7 @@ import express from 'express';
 const router = express.Router();
 import passport from 'passport';
 import * as authController from '../controllers/authController.js';
-import {authenticate}  from '../middleware/authMiddleware.js';
+//import {authenticate}  from '../middleware/authMiddleware.js';
 
 // User registration route
 router.post('/register', authenticate, authController.register);
@@ -12,9 +12,9 @@ router.post('/register', authenticate, authController.register);
 // User login route
 router.post('/login', authController.login);
 
-router.post('/user/refresh-token', authController.refreshToken);
+//router.post('/user/refresh-token', authController.refreshToken);
 
 // User logout route
-router.get('/logout', authenticate, authController.logout);
+//router.get('/logout', authenticate, authController.logout);
 
 export default router;
