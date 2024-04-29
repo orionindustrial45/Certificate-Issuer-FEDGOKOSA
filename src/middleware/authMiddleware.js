c25f5b787772785ee533016052635a08bf6ccf1f
orionindustrial45 <166864535+orionindustrial45@users.noreply.github.com>
2024-04-29 14:56:56 +0100

    Update authMiddleware.js

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,27 +1,27 @@
-import  jwt  from "jsonwebtoken";
-import dotenv from 'dotenv'
+// import  jwt  from "jsonwebtoken";
+// import dotenv from 'dotenv'
 
 
-dotenv.config()
+// dotenv.config()
 
 
-export const authenticate = async(req, res, next) => {
-    try {
-        const token = req.headers.authorization.split(' ')[1];
-        if(!token) {
-            return res.status(401).json({message: "Access denied. No token provided"});
-        }
-        const decoded = await jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-        req.user = decoded;
-        next();
+// export const authenticate = async(req, res, next) => {
+//     try {
+//         const token = req.headers.authorization.split(' ')[1];
+//         if(!token) {
+//             return res.status(401).json({message: "Access denied. No token provided"});
+//         }
+//         const decoded = await jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+//         req.user = decoded;
+//         next();
         
-    } catch (error) {
-        if(error.name === 'TokenExpiredError') {
-            return res.status(401).json({error: error.message});   
-        }
-        if (error.name === 'JsonWebTokenError') {
-            return res.status(401).json({ message: "Invalid token" });
-        }
-        res.status(500).json({ message: "Internal server error" });  
-    }
-}
+//     } catch (error) {
+//         if(error.name === 'TokenExpiredError') {
+//             return res.status(401).json({error: error.message});   
+//         }
+//         if (error.name === 'JsonWebTokenError') {
+//             return res.status(401).json({ message: "Invalid token" });
+//         }
+//         res.status(500).json({ message: "Internal server error" });  
+//     }
+// }
